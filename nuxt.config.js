a149403af3f6aa583bd340d11bae4aabb0fd59a7
madiodio <papigaye1@gmail.com>
2017-12-13 18:19:45 +0000

    💅🏽

---
+++ nuxt.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: "SenClick",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "comDigi" }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: "#3B8070" },
+  mode: "spa",
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/
+        });
+      }
+    }
+  }
+};
---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        SenClick 360
+      </h1>
+      <h2 class="subtitle">
+        Agence de communication digitale
+      </h2>
+      <h3 class="sub-subtitle">
+        Coming Soon!
+      </h3>
+      <div class="links">
+        <a href="http://madiodio.blogspot.sn/" target="_blank" class="button--green">Aller sur le blog</a>
+        <a href="mailto:papigaye1@gmail.com" target="_blank" class="button--grey">Contactez-nous</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  display: block;
+  font-weight: 600;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.sub-subtitle {
+  font-weight: 300;
+  font-size: 14px;
+  color: #526488;
+  text-transform: uppercase;
+  letter-spacing: 14px;
+  padding-bottom: 10px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
